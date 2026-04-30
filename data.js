e1fd6df347bc49fb947e81ef3125c4c29d324834
Onysuniverse <onysuniverse@gmail.com>
2026-04-30 02:49:43 +0000

    Change span for unnamed4.jpg to col-span-2

--- data.js
+++ data.js
@@ -57,6 +57,6 @@ export const galleryImages = [
     { url: '/unnamed9.jpg', span: 'col-span-1 row-span-1' },
     { url: '/unnamed7.jpg', span: 'col-span-1 row-span-1' },
     { url: '/unnamed3.jpg', span: 'col-span-1 row-span-1' },
-    { url: '/unnamed4.jpg', span: 'col-span-1 row-span-1' },
+    { url: '/unnamed4.jpg', span: 'col-span-2 row-span-1' },
     { url: '/unnamed5.jpg', span: 'col-span-1 row-span-1' }
 ];
